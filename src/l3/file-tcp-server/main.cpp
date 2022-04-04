f0f9c0834a940ea8526cb34c9d1319207e878401
hugo <brigan88@yandex.ru>
2022-04-04 14:28:57 +0500

    Lesson 3 clang-format

--- src/l3/file-tcp-server/main.cpp
+++ src/l3/file-tcp-server/main.cpp
@@ -8,20 +8,20 @@ extern "C"
 #include <sys/fcntl.h>
 #include <sys/sendfile.h>
 }
+#include <socket_wrapper/socket_class.h>
 #include <socket_wrapper/socket_headers.h>
 #include <socket_wrapper/socket_wrapper.h>
-#include <socket_wrapper/socket_class.h>
-
 
 // Trim from end (in place).
 static inline std::string& rtrim(std::string& s)
 {
-    s.erase(std::find_if(s.rbegin(), s.rend(), [](int c) { return !std::isspace(c); }).base());
+    s.erase(std::find_if(
+                s.rbegin(), s.rend(), [](int c) { return !std::isspace(c); })
+                .base());
     return s;
 }
 
-
-int main(int argc, char const *argv[])
+int main(int argc, char const* argv[])
 {
 
     if (argc != 2)
@@ -31,9 +31,9 @@ int main(int argc, char const *argv[])
     }
 
     socket_wrapper::SocketWrapper sock_wrap;
-    const int port { std::stoi(argv[1]) };
+    const int                     port{ std::stoi(argv[1]) };
 
-    socket_wrapper::Socket sock = {AF_INET, SOCK_STREAM, IPPROTO_TCP};
+    socket_wrapper::Socket sock = { AF_INET, SOCK_STREAM, IPPROTO_TCP };
 
     std::cout << "Starting echo server on the port " << port << "...\n";
 
@@ -43,10 +43,9 @@ int main(int argc, char const *argv[])
         return EXIT_FAILURE;
     }
 
-    sockaddr_in addr =
-    {
+    sockaddr_in addr = {
         .sin_family = AF_INET,
-        .sin_port = htons(port),
+        .sin_port   = htons(port),
     };
     addr.sin_addr.s_addr = INADDR_ANY;
     std::cout << "Test\n";
@@ -56,51 +55,54 @@ int main(int argc, char const *argv[])
         // Socket will be closed in the Socket destructor.
         return EXIT_FAILURE;
     }
-    listen (sock, 10);
-    
-    char buffer[256] = {0};
+    listen(sock, 10);
+
+    char buffer[256] = { 0 };
 
     // socket address used to store client address
-    struct sockaddr_storage client_address = {0};
-    socklen_t client_address_len = sizeof(sockaddr_in);
-    ssize_t recv_len = 0;
+    struct sockaddr_storage client_address     = { 0 };
+    socklen_t               client_address_len = sizeof(sockaddr_in);
+    ssize_t                 recv_len           = 0;
 
     std::cout << "Running echo server...\n" << std::endl;
     char client_address_buf[INET_ADDRSTRLEN];
-    int connect_socket = accept (sock, reinterpret_cast<sockaddr *>(&client_address),
-&client_address_len);
+    int  connect_socket = accept(sock,
+                                reinterpret_cast<sockaddr*>(&client_address),
+                                &client_address_len);
 
-        // Read content into buffer from an incoming client.
-        /*recv_len = recvfrom(sock, buffer, sizeof(buffer) - 1, 0,
-                            reinterpret_cast<sockaddr *>(&client_address),
-                            &client_address_len);*/
-    recv_len = recv (connect_socket, buffer, sizeof(buffer) - 1, 0); 
+    // Read content into buffer from an incoming client.
+    /*recv_len = recvfrom(sock, buffer, sizeof(buffer) - 1, 0,
+                        reinterpret_cast<sockaddr *>(&client_address),
+                        &client_address_len);*/
+    recv_len         = recv(connect_socket, buffer, sizeof(buffer) - 1, 0);
     std::string name = buffer;
-    int file = open (name.c_str(), O_RDONLY);
-    if (file <= 0) 
+    int         file = open(name.c_str(), O_RDONLY);
+    if (file <= 0)
     {
-    	std::cout <<"File is not exist\n";
-    	std::cout << file;
-    	return EXIT_FAILURE;
-    } 
-    
-	std::cout << file <<name.c_str();
-        if (recv_len > 0)
-        {
-             buffer[recv_len] = '\0';
-             std::cout << buffer << std::endl;
+        std::cout << "File is not exist\n";
+        std::cout << file;
+        return EXIT_FAILURE;
+    }
 
-            send(connect_socket, buffer, recv_len, 0);
-            off_t offset = 0;
-            int i = 0;
-            while (sendfile (connect_socket, file, &offset, 3) == 3) {std::cout << ++i << std::endl;}
-            int file_lenght = 3;
-            std::cout << file_lenght<<" "<<errno<<std::endl;
+    std::cout << file << name.c_str();
+    if (recv_len > 0)
+    {
+        buffer[recv_len] = '\0';
+        std::cout << buffer << std::endl;
+
+        send(connect_socket, buffer, recv_len, 0);
+        off_t offset = 0;
+        int   i      = 0;
+        while (sendfile(connect_socket, file, &offset, 3) == 3)
+        {
+            std::cout << ++i << std::endl;
         }
+        int file_lenght = 3;
+        std::cout << file_lenght << " " << errno << std::endl;
+    }
     ~sock.close();
-    close (connect_socket);
-        //std::cout << std::endl;
+    close(connect_socket);
+    // std::cout << std::endl;
 
     return EXIT_SUCCESS;
 }
-
--- src/l3/my_tcp_client/main.cpp
+++ src/l3/my_tcp_client/main.cpp
@@ -1,24 +1,25 @@
 #include <algorithm>
+#include <cassert>
 #include <cstdlib>
 #include <iomanip>
 #include <iostream>
 #include <string>
 #include <vector>
-#include <cassert>
 
+#include <socket_wrapper/socket_class.h>
 #include <socket_wrapper/socket_headers.h>
 #include <socket_wrapper/socket_wrapper.h>
-#include <socket_wrapper/socket_class.h>
 
 const size_t command_size = 5;
 
 // Trim from end (in place).
 static inline std::string& rtrim(std::string& s)
 {
-    s.erase(std::find_if(s.rbegin(), s.rend(), [](int c) { return !std::isspace(c); }).base());
+    s.erase(std::find_if(
+                s.rbegin(), s.rend(), [](int c) { return !std::isspace(c); })
+                .base());
     return s;
 }
-
 
 int main(int argc, const char* argv[])
 {
@@ -29,7 +30,7 @@ int main(int argc, const char* argv[])
     }
 
     socket_wrapper::SocketWrapper sock_wrap;
-    socket_wrapper::Socket sock = {AF_INET, SOCK_STREAM, IPPROTO_TCP};
+    socket_wrapper::Socket        sock = { AF_INET, SOCK_STREAM, IPPROTO_TCP };
 
     if (!sock)
     {
@@ -38,46 +39,52 @@ int main(int argc, const char* argv[])
     }
 
     const std::string host_name = { argv[1] };
- //   const struct hostent *remote_host { gethostbyname(host_name.c_str()) };
-    struct addrinfo hints;
-    struct addrinfo *result;
-    hints.ai_family = AF_UNSPEC;
-    hints.ai_socktype = SOCK_STREAM;
-    hints.ai_flags = AI_PASSIVE;
-    hints.ai_protocol = 0;
+    //   const struct hostent *remote_host { gethostbyname(host_name.c_str()) };
+    struct addrinfo  hints;
+    struct addrinfo* result;
+    hints.ai_family    = AF_UNSPEC;
+    hints.ai_socktype  = SOCK_STREAM;
+    hints.ai_flags     = AI_PASSIVE;
+    hints.ai_protocol  = 0;
     hints.ai_canonname = NULL;
-    hints.ai_addr = NULL;
-    hints.ai_next = NULL;
-    
-    if (getaddrinfo (host_name.c_str(), NULL, &hints, &result) != 0) 
+    hints.ai_addr      = NULL;
+    hints.ai_next      = NULL;
+
+    if (getaddrinfo(host_name.c_str(), NULL, &hints, &result) != 0)
     {
-    	std::cout<< "Node is not available\n";
-    	return EXIT_FAILURE;
+        std::cout << "Node is not available\n";
+        return EXIT_FAILURE;
     }
- /*   struct sockaddr_in server_addr =
-    {
-        .sin_family = AF_INET,
-        .sin_port = htons(std::stoi(argv[2]))
-    };*/
-    struct sockaddr_in server_addr = *reinterpret_cast<struct sockaddr_in*>(result->ai_addr);
+    /*   struct sockaddr_in server_addr =
+       {
+           .sin_family = AF_INET,
+           .sin_port = htons(std::stoi(argv[2]))
+       };*/
+    struct sockaddr_in server_addr =
+        *reinterpret_cast<struct sockaddr_in*>(result->ai_addr);
     server_addr.sin_port = htons(std::stoi(argv[2]));
-//    server_addr.sin_addr.s_addr = *reinterpret_cast<const in_addr_t*>(remote_host->h_addr);
+    //    server_addr.sin_addr.s_addr = *reinterpret_cast<const
+    //    in_addr_t*>(remote_host->h_addr);
     socklen_t server_address_len = sizeof(server_addr);
-    if (0 == connect(sock, reinterpret_cast<const sockaddr* const>(&server_addr), sizeof(server_addr)))
+    if (0 == connect(sock,
+                     reinterpret_cast<const sockaddr* const>(&server_addr),
+                     sizeof(server_addr)))
     {
-	        std::cout << "Connected to \"" << host_name << "\"..." << std::endl;
-    while (true)
-    {
-        std::string request;
-        std::vector<uint8_t> buffer;
-        buffer.resize(command_size);
-        char buf[256];
-     
+        std::cout << "Connected to \"" << host_name << "\"..." << std::endl;
+        while (true)
+        {
+            std::string          request;
+            std::vector<uint8_t> buffer;
+            buffer.resize(command_size);
+            char buf[256];
 
             std::cin >> request;
             request += "\n";
 
-            if (send(sock, request.c_str(), static_cast<int>(request.length()), 0) < 0)
+            if (send(sock,
+                     request.c_str(),
+                     static_cast<int>(request.length()),
+                     0) < 0)
             {
                 std::cerr << sock_wrap.get_last_error_string() << std::endl;
                 return EXIT_FAILURE;
@@ -85,10 +92,10 @@ int main(int argc, const char* argv[])
             recv(sock, &request[0], static_cast<size_t>(buffer.size()), 0);
             std::cout << request;
             std::cout << std::endl;
-            for (int i = 0; i < 10; i++) buf[i] = '\0';
-    }
+            for (int i = 0; i < 10; i++)
+                buf[i] = '\0';
+        }
     }
     sock.close();
-        return EXIT_FAILURE;
+    return EXIT_FAILURE;
 }
-
